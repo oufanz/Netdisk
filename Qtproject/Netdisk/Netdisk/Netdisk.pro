afa7805a9b2b1b84e5e170ecb52a89dedb3d1b8a
ou_fan <1119498859@qq.com>
2024-05-23 16:50:40 +0800

    4.网络封装

--- Qtproject/Netdisk/Netdisk/Netdisk.pro
+++ Qtproject/Netdisk/Netdisk/Netdisk.pro
@@ -8,6 +8,8 @@ CONFIG += c++11
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+include(./netapi/netapi.pri)
+INCLUDEPATH += ./netapi/
 SOURCES += \
     ckernel.cpp \
     main.cpp \
@@ -24,3 +26,5 @@ FORMS += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+
---
+++ Qtproject/Netdisk/Netdisk/netapi/net/packdef.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include<memory.h>
+
+#define _DEF_BUFFER         (4096)
+#define _DEF_CONTENT_SIZE	(1024)
+#define _MAX_SIZE           (40)
+#define _MAX_PATH           (260)
+
+//自定义协议   先写协议头 再写协议结构
+//登录 注册 获取好友信息 添加好友 聊天 发文件 下线请求
+#define _DEF_PACK_BASE	(10000)
+#define _DEF_PACK_COUNT (100)
+
+//注册
+#define _DEF_PACK_REGISTER_RQ	(_DEF_PACK_BASE + 0 )
+#define _DEF_PACK_REGISTER_RS	(_DEF_PACK_BASE + 1 )
+//登录
+#define _DEF_PACK_LOGIN_RQ	(_DEF_PACK_BASE + 2 )
+#define _DEF_PACK_LOGIN_RS	(_DEF_PACK_BASE + 3 )
+
+
+//返回的结果
+//注册请求的结果
+#define user_is_exist		(0)
+#define register_success	(1)
+//登录请求的结果
+#define user_not_exist		(0)
+#define password_error		(1)
+#define login_success		(2)
+
+
+typedef int PackType;
+
+//协议结构
+//注册
+typedef struct STRU_REGISTER_RQ
+{
+	STRU_REGISTER_RQ():type(_DEF_PACK_REGISTER_RQ)
+	{
+		memset( tel  , 0, sizeof(tel));
+		memset( name  , 0, sizeof(name));
+		memset( password , 0, sizeof(password) );
+	}
+	//需要手机号码 , 密码, 昵称
+	PackType type;
+	char tel[_MAX_SIZE];
+	char name[_MAX_SIZE];
+	char password[_MAX_SIZE];
+
+}STRU_REGISTER_RQ;
+
+typedef struct STRU_REGISTER_RS
+{
+	//回复结果
+	STRU_REGISTER_RS(): type(_DEF_PACK_REGISTER_RS) , result(register_success)
+	{
+	}
+	PackType type;
+	int result;
+
+}STRU_REGISTER_RS;
+
+//登录
+typedef struct STRU_LOGIN_RQ
+{
+	//登录需要: 手机号 密码 
+	STRU_LOGIN_RQ():type(_DEF_PACK_LOGIN_RQ)
+	{
+		memset( tel , 0, sizeof(tel) );
+		memset( password , 0, sizeof(password) );
+	}
+	PackType type;
+	char tel[_MAX_SIZE];
+	char password[_MAX_SIZE];
+
+}STRU_LOGIN_RQ;
+
+typedef struct STRU_LOGIN_RS
+{
+	// 需要 结果 , 用户的id
+	STRU_LOGIN_RS(): type(_DEF_PACK_LOGIN_RS) , result(login_success),userid(0)
+	{
+	}
+	PackType type;
+	int result;
+	int userid;
+
+}STRU_LOGIN_RS;
+
+
+
+
+
+
